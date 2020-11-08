77c7d9d6e26345c5be84d60dc108658aeec9b3d6
el <re.elfizan@gmail.com>
2020-11-08 12:01:35 +0700

    Upload Imgure

--- admin/src/stylesheet/js/manage-menu.js
+++ admin/src/stylesheet/js/manage-menu.js
@@ -15,7 +15,7 @@ $("#click-manage-menu").click(function () {
             console.log(data)
             let html = '';
             $("#content-menu").html(html);
-            data.forEach((dt, i) => { 
+            data.forEach((dt, i) => {
                 html = html + '<tr>' +
                     '<td class="text-left py-3 px-4">' + dt.name + '</td>' +
                     '<td class="text-left py-3 px-4">' + dt.link.substring(0, 20) + '...' + '</td>' +
@@ -23,7 +23,7 @@ $("#click-manage-menu").click(function () {
                     '<td class="text-center py-3 px-4"><button id="btn-update" value="' + dt._id + '"' +
                     '   class="inline-block text-sm px-4 py-2 leading-none border rounded text-white border-white bg-green-500 hover:border-transparent hover:text-teal-500 hover:bg-white mt-4 lg:mt-0"' +
                     '  type="submit">update</button>&nbsp;' +
-                    '<button id="btn-delete" value="' + dt._id + '"' +
+                    '<button id="btn-delete-menu" value="' + dt._id + '"' +
                     ' class="inline-block text-sm px-4 py-2 leading-none border rounded text-white border-white bg-red-500 hover:border-transparent hover:text-teal-500 hover:bg-white mt-4 lg:mt-0"' +
                     'type="submit">delete</button>' +
                     '</td>' +
@@ -45,4 +45,57 @@ $("#cancel-menu").click(function () {
 $("#btn-add-menu").click(function () {
     $("#table-menu").hide();
     $("#form-add-menu").show();
+});
+
+$("#save-menu").click(function (event) {
+    event.preventDefault();
+    const fd = new FormData();
+
+    let img = document.getElementById('file-menu');
+
+    fd.append('name', $("#name-menu").val());
+    fd.append('link', img.files[0]);
+    fd.append('price', $("#price-menu").val());
+
+    $.ajax({
+        url: `http://localhost:3000/menu/`,
+        headers: {
+            token: localStorage.getItem('token')
+        },
+        data: fd,//JSON.stringify(fd),
+        // data: {
+        //     name: $("#name-menu").val(),
+        //     link: fd,
+        //     price: $("#price-menu").val()
+        // },
+        processData: false,
+        contentType: false,
+        type: 'POST'
+    })
+        .done(function () {
+            alert('menu have been created');
+        })
+        .catch(function (error) {
+            alert('cant created menu : ' + error);
+        })
+});
+
+// $("#btn-delete-menu").on("click", function (event) {
+$(document).on("click", "#btn-delete-menu", function (event) {
+    console.log('masuk evebnt delete');
+    var id = $(this).val();
+    $.ajax({
+        url: `http://localhost:3000/menu/${id}`,
+        headers: {
+            token: localStorage.getItem('token')
+        },
+        type: 'DELETE'
+    })
+        .done(function (data) {
+            location.reload();
+            alert('delete success');
+        })
+        .catch(function (error) {
+            alert(error.message);
+        })
 });
---
+++ server/src/middleware/upload-imgur.js
@@ -0,0 +1,66 @@
+const multer = require('multer');
+const FormData = require('form-data');
+const imgur = require('imgur');
+
+// // Setting
+imgur.setClientId(process.env.CLIENTID_IMGUR);
+// imgur.setAPIUrl(process.env.ENDPOINT_IMGUR);
+// // Getting
+// imgur.getClientId();
+// imgur.getAPIUrl();
+
+// const instance = axios.create({
+//   baseURL: process.env.ENDPOINT_IMGUR,
+//   // timeout: 1000,
+//   headers: { Authorization: `Client-ID ${process.env.CLIENTID_IMGUR}`}
+// });
+
+function uploaded(req, res, next) {
+  var storage = multer.memoryStorage()
+  var upload = multer({
+    storage: storage
+  });
+
+  const uploadImage = upload.single('link');
+
+  uploadImage(req, res, function (err) {
+    if (err) {
+      // console.log(err);
+      return res.end('error: ', err);
+    }
+    else {
+      // let fd = new FormData();
+      // let stream = req.file.buffer.toString('base64');
+      // fd.append('image', stream);
+
+      //axios
+      // instance.post('/upload', fd)
+      //   .then((data) => {
+      //     console.log('data');
+      //     // next();
+      //   })
+      //   .catch((err) => {
+      //     console.log(err.message);
+      //   })
+
+      // req.files.forEach(function (item) {
+      //   img = item;
+      //   // console.log(item);
+      // });
+      // console.log(req.file);
+
+      imgur
+        .uploadBase64(req.file.buffer.toString('base64'))
+        .then(function (json) {
+          // console.log(json.data.link);
+          req.body.link=json.data.link;
+          next();
+        })
+        .catch(function (err) {
+          console.error(err.message);
+        });
+
+    };
+  })
+}
+module.exports = uploaded;
